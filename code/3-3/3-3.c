7cc2811242a046914b8200e9731a12042759c47d
alvin7187 <alvin060718@gmail.com>
2026-04-02 19:50:22 +0900

    Update 3-3.c

--- code/3-3/3-3.c
+++ code/3-3/3-3.c
@@ -2,10 +2,10 @@
 
 
 int main(){
-    printf("sizeof(*int) = %zu\n", sizeof(int*));
-    printf("sizeof(*double) = %zu\n", sizeof(double*));
-    printf("sizeof(*char) = %zu\n", sizeof(char));
-    printf("sizeof(*void) = %zu\n", sizeof(void*));
+    printf("sizeof(int*) = %zu\n", sizeof(int*));
+    printf("sizeof(double*) = %zu\n", sizeof(double*));
+    printf("sizeof(char*) = %zu\n", sizeof(char));
+    printf("sizeof(void*) = %zu\n", sizeof(void*));
 
     return 0;
 }
