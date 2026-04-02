a60199e6411fa6d4d91bb845a86a7cb6b2521267
alvin7187 <alvin060718@gmail.com>
2026-04-02 19:54:09 +0900

    Update 3-3.c

--- code/3-3/3-3.c
+++ code/3-3/3-3.c
@@ -4,7 +4,7 @@
 int main(){
     printf("sizeof(int*) = %zu\n", sizeof(int*));
     printf("sizeof(double*) = %zu\n", sizeof(double*));
-    printf("sizeof(char*) = %zu\n", sizeof(char));
+    printf("sizeof(char*) = %zu\n", sizeof(char*));
     printf("sizeof(void*) = %zu\n", sizeof(void*));
 
     return 0;
